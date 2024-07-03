7c30b98fa98acd19e8bf34027b61c32c8dc12df0
Sanawar Shoukat <>
2024-07-03 19:13:04 +0500

    Initial commit

---
+++ src/test/java/com/codelexer/springcloudawsses/SpringCloudAwsSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelexer.springcloudawsses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAwsSesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
